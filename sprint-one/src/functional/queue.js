947ccc0f4c64d49df60d22ddcd8d8a75af0d7e2b
jefelewis <jeffylew77@gmail.com>
2018-01-17 14:22:10 -0800

    Finsihed with Stack

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,39 @@
+// Queue: Last In First Out
+
 var Queue = function() {
   var someInstance = {};
 
-  // Use an object with numeric keys to store values
+  // Use an object with numeric keys to store values (Storage is the Queue)
   var storage = {};
+  // First Element
+  var first = 0;
+  // Last Element
+  var last = 0;
 
   // Implement the methods below
-
+  // Enqueue: Add to Back of Queue
   someInstance.enqueue = function(value) {
+    first += 1;
+    storage[last] = value;
   };
 
+  // Dequeue: Remove from Front of Queue
   someInstance.dequeue = function() {
+    // Check to see if the queue is not empty
+    if(first > 0){
+      // Assign the first element to the temp variable
+      var temp = storage[first];
+      // Delete the first element
+      delete storage[first]
+      // Reduce the first count by 1
+      first -= 1;
+      // Return the temp value, which was assigned as the first value
+      return temp;
+    }
   };
 
   someInstance.size = function() {
+    return first;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,35 @@
+// Stack: Last In First Out
+
 var Stack = function() {
   var someInstance = {};
 
-  // Use an object with numeric keys to store values
+  // Use an object with numeric keys to store values (Storage is the Stack)
   var storage = {};
+  // Last Element
+  var last = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    // Adds 1 to the Stack
+    last += 1;
+    storage[last] = value;
   };
 
   someInstance.pop = function() {
+    // Check to see if the stack is not empty
+    if(last > 0){
+      // Assign the last element to the temp variable
+      var temp = storage[last];
+      // Remove the last element from the stack
+      delete storage[last];
+      last -= 1;
+      // Return the last element that was assigned to the temp varaible
+      return temp;
+    }
   };
 
   someInstance.size = function() {
+    return last;
   };
 
   return someInstance;
