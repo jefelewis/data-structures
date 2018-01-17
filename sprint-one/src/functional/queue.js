8489f898ef04f3c35ca6fc2eb101891b0bfb2859
jefelewis <jeffylew77@gmail.com>
2018-01-17 14:31:47 -0800

    Finished Queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -12,27 +12,27 @@ var Queue = function() {
   // Implement the methods below
   // Enqueue: Add to Back of Queue
   someInstance.enqueue = function(value) {
-    firstElement += 1;
+    lastElement += 1;
     storage[lastElement] = value;
   };
 
   // Dequeue: Remove from Front of Queue
   someInstance.dequeue = function() {
     // Check to see if the queue is not empty
-    if(firstElement > 0){
+    if(lastElement - firstElement > 0){
+      // Reduce the firstElement count by 1
+      firstElement += 1;
       // Assign the firstElement element to the temp variable
       var temp = storage[firstElement];
       // Delete the firstElement element
       delete storage[firstElement]
-      // Reduce the firstElement count by 1
-      firstElement -= 1;
       // Return the temp value, which was assigned as the firstElement value
       return temp;
     }
   };
 
   someInstance.size = function() {
-    return firstElement;
+    return lastElement - firstElement;
   };
 
   return someInstance;
