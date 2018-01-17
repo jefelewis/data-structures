38a298809c82321be08c108dd1035064ba93c33a
jefelewis <jeffylew77@gmail.com>
2018-01-17 14:23:17 -0800

    Renamed variables

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,35 +5,34 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values (Storage is the Queue)
   var storage = {};
-  // First Element
-  var first = 0;
-  // Last Element
-  var last = 0;
+
+  var firstElement = 0;
+  var lastElement = 0;
 
   // Implement the methods below
   // Enqueue: Add to Back of Queue
   someInstance.enqueue = function(value) {
-    first += 1;
-    storage[last] = value;
+    firstElement += 1;
+    storage[lastElement] = value;
   };
 
   // Dequeue: Remove from Front of Queue
   someInstance.dequeue = function() {
     // Check to see if the queue is not empty
-    if(first > 0){
-      // Assign the first element to the temp variable
-      var temp = storage[first];
-      // Delete the first element
-      delete storage[first]
-      // Reduce the first count by 1
-      first -= 1;
-      // Return the temp value, which was assigned as the first value
+    if(firstElement > 0){
+      // Assign the firstElement element to the temp variable
+      var temp = storage[firstElement];
+      // Delete the firstElement element
+      delete storage[firstElement]
+      // Reduce the firstElement count by 1
+      firstElement -= 1;
+      // Return the temp value, which was assigned as the firstElement value
       return temp;
     }
   };
 
   someInstance.size = function() {
-    return first;
+    return firstElement;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -5,31 +5,31 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values (Storage is the Stack)
   var storage = {};
-  // Last Element
-  var last = 0;
+
+  var lastElement = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
     // Adds 1 to the Stack
-    last += 1;
-    storage[last] = value;
+    lastElement += 1;
+    storage[lastElement] = value;
   };
 
   someInstance.pop = function() {
     // Check to see if the stack is not empty
-    if(last > 0){
-      // Assign the last element to the temp variable
-      var temp = storage[last];
-      // Remove the last element from the stack
-      delete storage[last];
-      last -= 1;
-      // Return the last element that was assigned to the temp varaible
+    if(lastElement > 0){
+      // Assign the lastElement element to the temp variable
+      var temp = storage[lastElement];
+      // Remove the lastElement element from the stack
+      delete storage[lastElement];
+      lastElement -= 1;
+      // Return the lastElement element that was assigned to the temp varaible
       return temp;
     }
   };
 
   someInstance.size = function() {
-    return last;
+    return lastElement;
   };
 
   return someInstance;
