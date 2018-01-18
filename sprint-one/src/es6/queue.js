2f08945712fdb6b5a5d9532f351d1fd894a51165
jefelewis <jeffylew77@gmail.com>
2018-01-17 19:06:37 -0800

    Finished with ES6

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,32 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.firstElement = 0;
+    this.lastElement = 0;
+    this.storage = {};
+  }
+
+  enqueue(value) {
+    this.lastElement += 1;
+    this.storage[this.lastElement] = value;
+  }
+
+  // Dequeue: Remove from Front of Queue
+  dequeue() {
+    // Check to see if the queue is not empty
+    if(this.lastElement - this.firstElement > 0){
+      // Reduce the firstElement count by 1
+      this.firstElement += 1;
+      // Assign the firstElement element to the temp variable
+      var temp = this.storage[this.firstElement];
+      // Delete the firstElement element
+      delete this.storage[this.firstElement]
+      // Return the temp value, which was assigned as the firstElement value
+      return temp;
+    }
+  }
+  size() {
+    return this.lastElement - this.firstElement;
   }
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,33 @@
 class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
   constructor() {
+    this.lastElement = 0;
+    this.storage = {};
+  }
+
+  push(value) {
+    // Adds 1 to the Stack
+    this.lastElement += 1;
+    this.storage[this.lastElement] = value;
+  }
+
+  pop() {
+    // Check to see if the stack is not empty
+    if(this.lastElement > 0){
+      // Assign the lastElement element to the temp variable
+      var temp = this.storage[this.lastElement];
+      // Remove the lastElement element from the stack
+      delete this.storage[this.lastElement];
+      this.lastElement -= 1;
+      // Return the lastElement element that was assigned to the temp varaible
+      return temp;
+    }
+  }
+
+  size() {
+    return this.lastElement;
   }
 
 }
