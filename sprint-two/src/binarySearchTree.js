a2f324b0c447837399c825e3a18d9ce9ab593570
jefelewis <jeffylew77@gmail.com>
2018-01-18 10:20:03 -0800

    addToTail complete

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,16 @@
-var BinarySearchTree = function(value) {
+var BinarySearchTree = function(input, value) {
+  var startIndex = 0;
+  var middleIndex = (input.length / 2);
+  var endIndex = input.length
+
+  // If Value is greater than the middleIndex, Move to the Right
+  if(value > middleIndex){
+    startIndex = middleIndex;
+  }
+  // If the Value is less than the middleIndex, Move to the Left
+  else{
+    endIndex = middleIndex;
+  }
 };
 
 
--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,31 @@
 var LinkedList = function() {
   var list = {};
-  list.head = null;
-  list.tail = null;
+  list.head = 0;
+  list.tail = 0;
 
   list.addToTail = function(value) {
+    // Assign value to the Node using the Node function
+    list.tail = Node(value);
   };
 
   list.removeHead = function() {
+    // Check to see if the Linked List is empty
+    if(list.head !== 0){
+      var temp = list.head;
+      delete list.head;
+      return temp;
+    }
   };
 
   list.contains = function(target) {
+    for(var i = 0; i < list.length; i++){
+      if(list[i] === target){
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
   };
 
   return list;
