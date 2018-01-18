19f5a165e70010d87c93cccb18642be2b7e820c3
jefelewis <jeffylew77@gmail.com>
2018-01-18 14:20:10 -0800

    Finsished remove head

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,19 +1,28 @@
 var BinarySearchTree = function(input, value) {
   var startIndex = 0;
-  var middleIndex = (input.length / 2);
-  var endIndex = input.length
+  var middleIndex = Math.floor(input.length/2);
+  var endIndex = input.length;
 
-  // If Value is greater than the middleIndex, Move to the Right
-  if(value > middleIndex){
-    startIndex = middleIndex;
-  }
-  // If the Value is less than the middleIndex, Move to the Left
-  else{
-    endIndex = middleIndex;
-  }
+  // while(value !== middleIndex){
+  //   // If Value is greater than the middleIndex, Move to the Right
+  //   if(value > middleIndex){
+  //     startIndex = middleIndex;
+  //   }
+  //   // If the Value is less than the middleIndex, Move to the Left
+  //   else if(value < middleIndex){
+  //     endIndex = middleIndex;
+  //   }
+  //   // If the Value equals the middleIndexIndex, return the value
+  //   else{
+  //     return BinarySearchTree();
+  //   }
+  // }
 };
 
+// console.log(BinarySearchTree([1,2,3,4,5,6,7,8,9,10], 7));
 
 /*
- * Complexity: What is the time complexity of the above functions?
- */
+Complexity: What is the time complexity of the above functions?
+
+
+*/
--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,33 +1,53 @@
 // Linked List Function (Contains the Node objects)
 var LinkedList = function() {
   var list = {
-    head: 0,
-    tail: 0
+    head: null,
+    tail: null,
+    total: 0
   };
 
+
   list.addToTail = function(value) {
-    // Assign value to the Node using the Node function
-    list.tail = Node(value);
+    // Check if Linked List has no nodes (Node becomes Head and Tail)
+    if(list.head === null){
+      // Assign value to the Head
+      list.head = Node(value);
+      // Assign Head to the Tail (We CANNOT assign list.tail to Node(value) because it creates TWO objects. They are the same output, but DIFFERENT objects)
+      list.tail = list.head;
+      // Add 1 to the total count
+      list.total += 1;
+    }
+
+    // If the Linked List has node
+    else if(list.head !== null){
+      // Assign Node to the Next (This needs to come before assigning it it the Tail)
+      list.tail.next = Node(value);
+      // Assign Node to the Tail
+      list.tail = list.tail.next;
+      // Add 1 to the total
+      list.total += 1;
+    }
   };
 
   list.removeHead = function() {
-    // Check to see if the Linked List is empty
-    if(list.head !== 0){
-      var temp = list.head;
-      delete list.head;
-      return temp;
+    // // Check if Linked List has no nodes
+    // if(list.head === null){
+    //   // Return an empty object Node?
+    //   return {};
+    // }
+    // Check if the Linked List has nodes
+    if(list.head !== null){
+      // Assign Head to value (Why Do I need value?)
+      var deletedHead = list.head.value;
+      // Assign the Element before the Head as the New Head (The previous head is overwritten)
+      list.head = list.head.next
+      // Remove the Head
+      return deletedHead;
     }
   };
 
   list.contains = function(target) {
-    // for(var i = 0; i < list.length; i++){
-    //   if(list[i] === target){
-    //     return true;
-    //   }
-    //   else{
-    //     return false;
-    //   }
-    // }
+
   };
 
   return list;
@@ -44,5 +64,8 @@ var Node = function(value) {
 };
 
 /*
- * Complexity: What is the time complexity of the above functions?
- */
+Complexity: What is the time complexity of the above functions?
+
+O(n) Linear
+
+*/
