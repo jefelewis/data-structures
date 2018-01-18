36f9a8e4facae34d03bd3ddd0015881c984a834b
jefelewis <jeffylew77@gmail.com>
2018-01-17 18:17:19 -0800

    Finsished with Pseudoclassical Stack + Queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,32 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  this.firstElement = 0;
+  this.lastElement = 0;
+  this.storage = {};
+
 };
 
-
+// METHODS
+Queue.prototype.enqueue = function(value) {
+  this.lastElement += 1;
+  this.storage[this.lastElement] = value;
+}
+// Dequeue: Remove from Front of Queue
+Queue.prototype.dequeue = function() {
+  // Check to see if the queue is not empty
+  if(this.lastElement - this.firstElement > 0){
+    // Reduce the firstElement count by 1
+    this.firstElement += 1;
+    // Assign the firstElement element to the temp variable
+    var temp = this.storage[this.firstElement];
+    // Delete the firstElement element
+    delete this.storage[this.firstElement]
+    // Return the temp value, which was assigned as the firstElement value
+    return temp;
+  }
+}
+Queue.prototype.size = function() {
+  return this.lastElement - this.firstElement;
+}
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,33 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  this.lastElement = 0;
+  this.storage = {};
+
 };
 
 
+// METHODS
+Stack.prototype.push = function(value) {
+  // Adds 1 to the Stack
+  this.lastElement += 1;
+  this.storage[this.lastElement] = value;
+},
+
+Stack.prototype.pop = function() {
+  // Check to see if the stack is not empty
+  if(this.lastElement > 0){
+    // Assign the lastElement element to the temp variable
+    var temp = this.storage[this.lastElement];
+    // Remove the lastElement element from the stack
+    delete this.storage[this.lastElement];
+    this.lastElement -= 1;
+    // Return the lastElement element that was assigned to the temp varaible
+    return temp;
+  }
+},
+
+Stack.prototype.size = function() {
+  return this.lastElement;
+}
