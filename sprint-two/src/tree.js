522b4a9c3d7842efbc020d31deb23bd2ada64c8e
jefelewis <jeffylew77@gmail.com>
2018-01-19 14:59:53 -0800

    Added recursion to iterate through the nodes. 1 test still failing

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,7 +1,12 @@
+// Function: Create New Tree
 var Tree = function(value) {
-  var newTree = {};
-  newTree.value = value;
-  newTree.children = [];
+  // var newTree = {};
+  // newTree.value = value;
+  // newTree.children = [];
+  var newTree = {
+    value: value,
+    children: []
+  };
 
   // Use Underscore Extend to Apply the Methods form the Source Object to Tree Object
   // Underscore: _.extend(object, source object)
@@ -16,43 +21,49 @@ var treeMethods = {};
 
 // Function: Add Child to Tree
 treeMethods.addChild = function(value) {
-  newTree.children.push(value);
+  // Push the value to the children array, but it has to be contained in an object
+  // REVISIT: WHY "THIS"
+  this.children.push(Tree(value));
 };
 
 // Function: Tree Contains Target
 treeMethods.contains = function(target) {
-  //
-  // var wasFound = false;
-  //
-  // var searchTree = function(node){
-  //
-  //   // BASE CASE
-  //   if(newTree.value === target){
-  //     wasFound === true;
-  //   }
-  //
-  //   // RECURSIVE CASE
-  //   if(Array.isArray(newTree)){
-  //     for(var i = 0; i < children.length; i++){
-  //       if(children[i] === target){
-  //         wasFound === true;
-  //       }
-  //       else{
-  //         searchTree()
-  //       }
-  //     }
-  //   }
-  // }
-  //
-  // // Call function
-  // contains(newTree);
-  // // Return Found boolean
-  // return wasFound;
+
+  var wasFound = false;
+
+  var searchNodes = function(node){
+
+    // BASE CASE
+    if(node.value === target){
+      wasFound === true;
+    }
+
+    // RECURSIVE CASE
+    if(Array.isArray(node.children)){
+      // Iterate through the Children array
+      for(var i = 0; i < node.children.length; i++){
+        // Check if Node Value equals Target
+        if(node.children[i].value === target){
+          wasFound = true;
+        }
+        // Check the Node has children array
+        if(Array.isArray(node.children[i])){
+          searchNodes(node.children[i]);
+        }
+      }
+    }
+  }
+
+  // Run the function, starting from the top of the tree
+  // REVISIT: WHY "THIS"
+  searchNodes(this);
+  return wasFound;
 };
 
 
 
 /*
 Complexity: What is the time complexity of the above functions?
+O(n) Linear
 
 */
