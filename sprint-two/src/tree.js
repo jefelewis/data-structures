01747682d460fc90db6b213bf03b06c8c1c36f9c
jefelewis <jeffylew77@gmail.com>
2018-01-19 14:07:48 -0800

    Used extend to apply the methods to the source object

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,8 +1,11 @@
 var Tree = function(value) {
-  var newTree = {
-    value: value,
-    children: []
-  };
+  var newTree = {};
+  newTree.value = value;
+  newTree.children = [];
+
+  // Use Underscore Extend to Apply the Methods form the Source Object to Tree Object
+  // Underscore: _.extend(object, source object)
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,12 +14,40 @@ var Tree = function(value) {
 // Tree Methods
 var treeMethods = {};
 
+// Function: Add Child to Tree
 treeMethods.addChild = function(value) {
-
+  newTree.children.push(value);
 };
 
+// Function: Tree Contains Target
 treeMethods.contains = function(target) {
-
+  //
+  // var wasFound = false;
+  //
+  // var searchTree = function(node){
+  //
+  //   // BASE CASE
+  //   if(newTree.value === target){
+  //     wasFound === true;
+  //   }
+  //
+  //   // RECURSIVE CASE
+  //   if(Array.isArray(newTree)){
+  //     for(var i = 0; i < children.length; i++){
+  //       if(children[i] === target){
+  //         wasFound === true;
+  //       }
+  //       else{
+  //         searchTree()
+  //       }
+  //     }
+  //   }
+  // }
+  //
+  // // Call function
+  // contains(newTree);
+  // // Return Found boolean
+  // return wasFound;
 };
 
 
