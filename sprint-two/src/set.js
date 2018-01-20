45a6dd430c96b1a0de8496ecd08b678d12d38953
jefelewis <jeffylew77@gmail.com>
2018-01-19 18:08:35 -0800

    Finished with Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -2,22 +2,31 @@
 
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = {}; // Fix me
+  set.storage = {}; // Fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
-  this.item = item;
+  this.storage[item] = {};
 };
 
 setPrototype.contains = function(item) {
-
+  // If the Node within the Nodes object is not undefined, return true
+  if(this.storage[item] !== undefined){
+    return true;
+  }
+  else{
+    return false;
+  }
 };
 
 setPrototype.remove = function(item) {
-
+  // If the Node within the Nodes object is not undefined, return true
+  if(this.storage[item] !== undefined){
+    delete this.storage[item];
+  }
 };
 
 /*
